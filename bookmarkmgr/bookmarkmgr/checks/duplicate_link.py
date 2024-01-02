6e5c9c91eba94545335a492a1047833197ef7d98
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2024-01-02 23:24:29 +0100

    bookmarkmgr: Deduplicate link duplication check logic

--- bookmarkmgr/bookmarkmgr/checks/duplicate_link.py
+++ bookmarkmgr/bookmarkmgr/checks/duplicate_link.py
@@ -1,6 +1,17 @@
 from asyncio import Event
 from datetime import datetime
-from types import SimpleNamespace
+
+
+class _Link:
+    def __init__(self, raindrop):
+        self.created = datetime.fromisoformat(raindrop["created"])
+        self.id = raindrop["_id"]
+
+
+def _is_first_link_older(link1, link2):
+    return link1.created < link2.created or (
+        link1.created == link2.created and link1.id < link2.id
+    )
 
 
 class DuplicateLinkChecker:
@@ -9,32 +20,24 @@ class DuplicateLinkChecker:
         self._original_links = {}
 
     def add_link(self, raindrop):
-        _id = raindrop["_id"]
-        created = datetime.fromisoformat(raindrop["created"])
         url = raindrop["link"]
         original_link = self._original_links.get(url)
+        tested_link = _Link(raindrop)
 
-        if (
-            original_link is None
-            or created < original_link.created
-            or (created == original_link.created and _id < original_link.id)
+        if original_link is None or _is_first_link_older(
+            tested_link,
+            original_link,
         ):
-            self._original_links[url] = SimpleNamespace(
-                created=created,
-                id=_id,
-            )
+            self._original_links[url] = tested_link
 
     async def is_link_duplicate(self, raindrop):
         if not self._all_links_received.is_set():
             await self._all_links_received.wait()
 
-        created = datetime.fromisoformat(raindrop["created"])
         original_link = self._original_links[raindrop["link"]]
+        tested_link = _Link(raindrop)
 
-        return original_link.created < created or (
-            original_link.created == created
-            and original_link.id < raindrop["_id"]
-        )
+        return _is_first_link_older(original_link, tested_link)
 
     def set_all_links_received(self):
         self._all_links_received.set()
