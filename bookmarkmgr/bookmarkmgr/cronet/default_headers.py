7f6e7ba7765e4b289d77e8003a9bc46c4dd496fa
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2025-04-02 13:19:15 +0200

    bookmarkmgr: Update default Cronet headers

--- bookmarkmgr/bookmarkmgr/cronet/default_headers.py
+++ bookmarkmgr/bookmarkmgr/cronet/default_headers.py
@@ -3,7 +3,7 @@
 DEFAULT_HEADERS = (
     (
         "sec-ch-ua",
-        '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
     ),
     ("sec-ch-ua-mobile", "?0"),
     ("sec-ch-ua-platform", '"Linux"'),
@@ -11,7 +11,7 @@ DEFAULT_HEADERS = (
     ("Upgrade-Insecure-Requests", "1"),
     (
         "User-Agent",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
     ),
     (
         "Accept",
